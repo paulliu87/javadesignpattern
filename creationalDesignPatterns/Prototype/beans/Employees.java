ccefe40cd300c17dce4edb867a5bbe7bf6024f8c
paulliu87 <paul.pliu87@gmail.com>
2018-05-21 23:59:55 -0700

    init files

---
+++ creationalDesignPatterns/Prototype/beans/Employees.java
@@ -0,0 +1,36 @@
+package beans;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Employees implements Cloneable {
+    private List<String> empList;
+
+    public Employees() {
+        empList = new ArrayList<String>();
+    }
+
+    public Employees(List<String> list) {
+        this.empList = list;
+    }
+
+    public void loadData( {
+        empList.add("Pankaj");
+        empList.add("Raj");
+        empList.add("John");
+        empList.add("Paul");
+    })
+
+    public List<String> getEmpList() {
+        return empList;
+    }
+
+    @Override
+    public Object clone() throws CloneNotSupportException {
+        List<String> temp = new ArrayList<String>();
+        for (String s : this.getEmpList()) {
+            temp.add(s);
+        }
+        return new Employees(temp);
+    }
+}
---
+++ creationalDesignPatterns/Prototype/test/EmployeesTest.java
@@ -0,0 +1,23 @@
+package test;
+
+import java.util.List;
+import beans.Employees;
+
+public class EmployeesTest {
+    public static void main(String[] args) throws CloneNotSupportedException {
+        Employees emps = new Employees();
+        emps.loadData();
+
+        Employees empsNew1 = (Employees) emps.clone();
+        Employees empsNew2 = (Employees) emps.clone();
+
+        List<String> list1 = empsNew1.getEmpList();
+        list1.add("Teng");
+        List<Stirng> list2 = empsNew2.getEmpList();
+        list2.remove("Paul");
+
+        System.out.println("emps List:" + emps.getEmpList());
+        System.out.println("empsNew1 List:" + list1);
+        System.out.println("empsNew2 List:" + list2);
+    }
+}
