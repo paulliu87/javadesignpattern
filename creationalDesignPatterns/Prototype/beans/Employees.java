f78734ea6edfb09b78c93208a8642ea65707d97f
paulliu87 <paul.pliu87@gmail.com>
2018-05-22 00:24:49 -0700

    compile

--- creationalDesignPatterns/Prototype/beans/Employees.java
+++ creationalDesignPatterns/Prototype/beans/Employees.java
@@ -14,19 +14,19 @@ public class Employees implements Cloneable {
         this.empList = list;
     }
 
-    public void loadData( {
+    public void loadData() {
         empList.add("Pankaj");
         empList.add("Raj");
         empList.add("John");
         empList.add("Paul");
-    })
+    }
 
     public List<String> getEmpList() {
         return empList;
     }
 
     @Override
-    public Object clone() throws CloneNotSupportException {
+    public Object clone() throws CloneNotSupportedException {
         List<String> temp = new ArrayList<String>();
         for (String s : this.getEmpList()) {
             temp.add(s);
